3714e6e2664cfa3c47149aeb7e2047bd079cb1d0
czesuaf <damian.czechowski@bloomnet.eu>
2018-07-10 20:19:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+## Quick Start
+```
+# 1 Clone this repo
+git clone https://github.com/wapbamboogie/bootstrap-4-boilerplate.git
+
+# 2 Navigate into the repo directory
+cd html5-bootstrap4-starter
+
+# 3 Install all node packages
+npm install
+
+# 4 Get started
+gulp serve - starts localhost server with browser-sync, watches HTML, Sass, JS with hot reloading
+gulp - minify CSS/JS and builds your app into the dist directory, ready for production
+```
+
+## Requirements
+This project requires you have [nodejs](https://nodejs.org/en/) with [npm](https://www.npmjs.com/get-npm) installed.
+This project requires you have a global installation of [gulp](http://gulpjs.com/).
+```
+# Install gulp globally
+npm install -g gulp
+```
+
+## Gulp commands
+**gulp serve**
+
+The gulp serve command starts a local Browsersync server that serves your files in the browser.
+It reloads the current page when changing HTML, PHP, Sass and JS files.
+The output of all Sass files go to main.css
+All JS files are concatenated into main.js
+You can access the development server with other devices on the same network. Go to the "External" address specified by Browsersync (see the terminal) in the web browser of your device.
+```
+gulp serve
+```
+
+**gulp (build)**
+
+The default gulp command is set to creating a "dist" directory with a production version of the project, ready to be deployed.
+It minifies and renames JS/CSS assets as well as cleaning the old "dist" directory. CSS is autoprefixed for the latest two browser versions.
+```
+gulp
+```
+
+**gulp concatScripts**
+
+The gulp concatScripts command combines the specified JS resources into main.js
+You can add new JS files to this command on line 16 in gulpfile.js
+You might want to run concatScripts once separately after adding new JS files.
+```
+gulp concatScripts
+```
+
+## Overwriting Bootstrap sass variables
+You can overwrite specific bootstrap sass variables by uncommenting lines in assets/css/1-frameworks/bootstrap/bootstrap-user-variables.scss
+
+## PHP
+If you need a server with PHP support you can use the project with [MAMP Pro](https://www.mamp.info/en/mamp-pro/) or similar.
+* Create a new host in the hosts panel of MAMP Pro and choose the project folder as the document root.
+* Enable Symlinks settings in the "Extended" tab of the host configuration.
+* On line 82 in gulpfile.js change the browserSync.init function to the following:
+```
+browserSync.init({
+    proxy: "http://name-of-your-mamp-host:8888",
+    open: "external"
+});
+```
+* Start the MAMP services and run "gulp serve" as before.
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+"use strict";
+
+var gulp = require('gulp'),
+	  concat = require('gulp-concat'),
+	  uglify = require('gulp-uglify'),
+	  rename = require('gulp-rename'),
+	  sass = require('gulp-sass'),
+	  maps = require('gulp-sourcemaps'),
+	  del = require('del'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  browserSync = require('browser-sync').create(),
+	  htmlreplace = require('gulp-html-replace'),
+	  cssmin = require('gulp-cssmin');
+
+gulp.task("concatScripts", function() {
+	return gulp.src([
+		'src/assets/js/vendor/jquery-3.3.1.slim.min.js',
+		'src/assets/js/vendor/popper.min.js',
+		'src/assets/js/vendor/bootstrap.min.js',
+		'src/assets/js/functions.js',
+		'src/assets/js/scripts.js'
+	])
+		.pipe(maps.init())
+		.pipe(concat('main.js'))
+		.pipe(maps.write('./src/'))
+		.pipe(gulp.dest('src/assets/js'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("src/assets/js/main.js")
+	  .pipe(uglify())
+	  .pipe(rename('main.min.js'))
+	  .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("src/assets/css/main.scss")
+    .pipe(maps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('src/assets/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("minifyCss", ["compileSass"], function() {
+  return gulp.src("src/assets/css/main.css")
+    .pipe(cssmin())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('src/assets/css/**/*.scss', ['compileSass']);
+  gulp.watch('src/assets/js/*.js', ['concatScripts']);
+})
+
+gulp.task('clean', function() {
+  del(['dist', 'src/assets/css/main.css*', 'assets/js/main*.js*']);
+});
+
+gulp.task('renameSources', function() {
+  return gulp.src(['*.html', '*.php'])
+    .pipe(htmlreplace({
+      'js': 'assets/js/main.min.js',
+      'css': 'assets/css/main.min.css'
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task("build", ['minifyScripts', 'minifyCss'], function() {
+  return gulp.src([
+		'src/*.html',
+		'src/*.php',
+		'src/favicon.ico',
+		"src/assets/img/**"
+	], { base: './src/'})
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles'], function(){
+  browserSync.init({
+  	server: "./src/"
+  });
+
+  gulp.watch("src/assets/css/**/*.scss", ['watchFiles']);
+  gulp.watch(['src/*.html', 'src/*.php']).on('change', browserSync.reload);
+});
+
+gulp.task("default", ["clean", 'build'], function() {
+  gulp.start('renameSources');
+});
